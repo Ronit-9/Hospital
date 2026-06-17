9c8e34a1f36da4a98bab98385a0e5cd71f7fd64e
Ronit Khadka <ronitkhadka4@gmail.com>
2026-06-17 16:40:43 +0545

    credential for cookies update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express()
 connectDB()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://hospital-vmds.onrender.com'],
   credentials: true,
 }))
 app.use(express.json())
