2e071364b692a3f1cb8285150bfb3a220d0a5754
Ronit Khadka <ronitkhadka4@gmail.com>
2026-06-17 17:02:07 +0545

    simple changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,9 +10,9 @@ const generateToken = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days
-  })
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
   return token
 }
